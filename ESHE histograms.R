5e4144cefe44f51cc1ae2ee263161c9295120061
Emily Raney <emilyraney@wireless-10-147-69-0.public.utexas.edu>
2021-09-17 17:20:03 -0500

    commit_sep17

--- ESHE histograms.R
+++ ESHE histograms.R
@@ -9,16 +9,16 @@ head(df)
 df
 hist1 <- ggplot(df, aes(x=year)) + geom_histogram(binwidth = 1, color = "darkorange4", fill = "darkorange3")
 hist1 + labs(title = "Nomina by year", 
-             x = "Year", y = "Nomina")
+             x = "Year", y = "Nomina") + theme_minimal()
 hist2 <- ggplot(df, aes(x=year)) + geom_histogram(binwidth = 2, color = "darkorange4", fill = "darkorange3")
 hist2 + labs(title = "Nomina by year", 
-             x = "Year", y = "Nomina")
+             x = "Year", y = "Nomina") + theme_minimal()
 hist5  <- ggplot(df, aes(x=year)) + geom_histogram(binwidth = 5, color = "darkorange4", fill = "darkorange3")
 hist5 + labs(title = "Nomina by year", 
-             x = "Year", y = "Nomina")
+             x = "Year", y = "Nomina") + theme_minimal()
 hist10 <- ggplot(df, aes(x=year)) + geom_histogram(binwidth = 10, color = "darkorange4", fill = "darkorange3")
 hist10 + labs(title = "Nomina by year", 
-              x = "Year", y = "Nomina")
+              x = "Year", y = "Nomina") + theme_minimal()
 
 g <- curl("https://raw.githubusercontent.com/raneyej/ESHE-2021/main/activenomina.csv")
 h <- read.csv(g, header = TRUE, sep = ",")
@@ -27,10 +27,10 @@ dfh <- data.frame(h)
 head(df2)
 active_hist10 <- ggplot(dfh, aes(x=year)) + geom_histogram(binwidth = 10, color = "darkorange4", fill = "darkorange3")
 active_hist10 + labs(title = "Active nomina by year", 
-             x = "Year", y = "Nomina")
+                     x = "Year", y = "Nomina") + theme_minimal()
 active_hist5 <- ggplot(dfh, aes(x=year)) + geom_histogram(binwidth = 5, color = "darkorange4", fill = "darkorange3")
 active_hist5 + labs(title = "Active nomina by year", 
-             x = "Year", y = "Nomina")
+                    x = "Year", y = "Nomina") + theme_minimal()
 
 p <- curl("https://raw.githubusercontent.com/raneyej/ESHE-2021/main/line4maybe.csv")
 q <- read.csv(p, header = TRUE, sep = ",")
@@ -39,3 +39,4 @@ dfq <- data.frame(q)
 head(dfq)
 line3 <- ggplot(dfq, aes(x=year, y=count)) + geom_line(color = "darkorange3") + geom_point(color = "darkorange4")
 line3 + labs(title= "Nomina by year", x = "Year", y = "Nomina") + theme_minimal() 
+ 
